d9d45111a12deb6194bcbc6562a6e9713e22f8c1
LAPTOP-IDJ82BKU\micha <michael.byalsky@gmail.com>
2020-11-10 15:25:39 +0200

    working version

---
+++ trump_selenium.py
@@ -0,0 +1,80 @@
+from time import sleep
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import numpy as np
+import re
+
+def cleanhtml(raw_html):
+  cleanr = re.compile('<.*?>')
+  cleantext = re.sub(cleanr, '', raw_html)
+  return cleantext
+
+def page_is_loading(driver):
+    while True:
+        x = driver.execute_script("return document.readyState")
+        if x == "complete":
+            return True
+        else:
+            yield False
+
+
+
+def find_trump():
+    PATH = 'C:\Program Files (x86)\chromedriver.exe'
+
+    pages = np.arange(0, 100220, 10)
+
+    driver = webdriver.Chrome(PATH)
+    i = 0
+    for page in pages:
+        i = i + 1
+        try:
+            driver.get(f'https://edition.cnn.com/search?q=trump&size={10}&from={page}&page={i}')
+            print(driver.title)
+            WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "cnn-search__results-list")))
+            while not page_is_loading(driver):
+                continue
+            sleep(2)
+            links = driver.find_elements_by_class_name("cnn-search__result-contents")
+            for z in range(0, len(links)):
+                try:
+                    WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "cnn-search__result-thumbnail")))
+                    driver.implicitly_wait(60)
+                    links2 = driver.find_elements_by_class_name("cnn-search__result-thumbnail")
+                    driver.implicitly_wait(60)
+                    links2[z].click()
+                    while not page_is_loading(driver):
+                        continue
+                    sleep(2)
+                    WebDriverWait(driver, 20).until(
+                        EC.presence_of_element_located((By.TAG_NAME, "body")))
+                except Exception as e:
+                    print("click error:", e)
+                sleep(2)
+                pageSource = driver.page_source
+                cleand = cleanhtml(pageSource)
+                splitedSource = cleand.split(".")
+                for sentence in splitedSource:
+                    splitedSentence = sentence.split()
+                    for word in splitedSentence:
+                        if word.lower() == 'trump':
+                            try:
+                                f = open("trump.txt", "a")
+                                sentenceToWright = " ".join(splitedSentence)
+                                f.write(sentenceToWright + "\n")
+                                f.close()
+                            except:
+                                print("problem2")
+                driver.get(f'https://edition.cnn.com/search?q=trump&size={10}&from={page}&page={i}')
+                while not page_is_loading(driver):
+                    continue
+
+        except Exception as e:
+            print(e)
+            driver.quit()
+
+
+if __name__ == '__main__':
+    find_trump()
